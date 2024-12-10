2fd0fa8946e9cf059d713569298fd325fdca5276
Afif Fahmi <afif.rafpi@gmail.com>
2024-12-10 21:09:31 +0800

    Fixed Android

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
-  
+const { wrapWithReanimatedMetroConfig } = require('react-native-reanimated/metro-config');
+
+// Get the default Metro configuration
 const config = getDefaultConfig(__dirname);
-  
-module.exports = withNativeWind(config, { input: './global.css' });
+
+// Apply NativeWind and Reanimated configurations
+module.exports = wrapWithReanimatedMetroConfig(
+  withNativeWind(config, { input: './global.css' })
+);
